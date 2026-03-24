aef1628e1bca952f89b797e7d7a5c255753beff8
Andy R. Schmitz <github@arschmitz.com>
2026-03-24 15:13:05 -0400

    Add the ability to post a meme directly

--- frontend/src/routes/meme/+page.server.ts
+++ frontend/src/routes/meme/+page.server.ts
@@ -1,7 +1,48 @@
-import type { ServerPageLoad } from './$types';
-import { redirect } from '@sveltejs/kit';
+import type { ServerPageLoad, Actions } from './$types';
+import { redirect, fail } from '@sveltejs/kit';
+import { CreatePostAndUpload } from '$lib/server/create_post.ts';
 
 export const load: ServerPageLoad = async({ locals }) => {
 	if(!locals.logged_in){redirect(307, '/login')}
 	return locals;
 }
+
+export const actions:Actions = {
+	upload: async({ locals, request, fetch, clientAddress }) => {
+		if(!locals.logged_in){redirect(307, '/login')}
+
+		const form_data = await request.formData();
+		const template_id = Number.parseInt(form_data.get('id'));
+		const file = form_data.get('file');
+		
+		const post_link = await CreatePostAndUpload(locals.user_id, file, {mime: 'image/svg+xml'});
+		if(post_link){
+			redirect(307, '/view/'+post_link);
+			//return '/view/'+post_link;
+		} else {
+			fail(400, {err_msg: 'There was an unknown problem uploading the file'});
+		}
+
+		//if(file && file.size > 0) {
+		//	//console.log(file);
+		//
+		//	if(file.size > MAX_FILE_SIZE){
+		//		return fail(400, {err_msg:'File is too large, over '+(MAX_FILE_SIZE/1024/1024).toFixed(0)+' MB.'});
+		//	}
+		//	let verified_type = await fileTypeFromBlob(file);
+		//	if(!ALLOWED_MIME_TYPES.includes(verified_type.mime)) {
+		//		//console.log('FAILED type');
+		//		//console.log(verified_type);
+		//		return fail(400, {err_msg:'Unsupported file type'});
+		//	}
+		//
+		//	let id = await CreatePostAndUpload(locals.user_id, file, verified_type);
+		//	if(id) {
+		//		redirect(307, '/view/' + id);
+		//		//return {success:true, post_id:id}
+		//	} else {
+		//		return fail(400, {err_msg: 'There was an unknown problem uploading the file'})
+		//	}
+		//}
+	}
+} satisfies Actions;
--- frontend/src/routes/meme/+page.svelte
+++ frontend/src/routes/meme/+page.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import TemplateSelector from './TemplateSelector.svelte';
 	import { get_width, split_text } from './MemeMaker.ts';
 
-	const PADDING=20;
+	const PADDING=10;
 	const data = $props();
 	let selected_meme = $state(undefined);
 	let text_blocks = $state([]);
 	let text_split = $state([]);
-	let font_size = $state(28);
+	let font_size = $state(48);
 	let dl_link = $state(undefined);
 
 	const update_meme=(values)=>{
@@ -53,22 +54,60 @@
 		}
 	};
 
+	const post = async(e)=>{
+		// Original, linking
+		//const clone=document.getElementById('svg').cloneNode(true);
+
+		// TODO TODO TODO TODO TODO TODO TODO TODO TODO
+		// Quickfix to DL and embed it (like we do with downloads)
+		// SVGs with image links work when embedded into the page, but not when
+		// linked within an <img src='blah.svg' /> element. But embedding it
+		// directly in the page leads to XSS concerns. So we need better validation
+		// before we can do (proper) linking. For now... DL & inline the bytes.
+		const d=await getFileAsBase64('/api/img/'+selected_meme.image);
+		const clone=document.getElementById('svg').cloneNode(true);
+		clone.getElementsByTagName('image')[0].setAttribute('href', 'data:'+d.t+';base64,'+d.d);
+
+		const data = (new XMLSerializer()).serializeToString(clone);
+		const blob = new Blob([data], {type: 'image/svg+xml;charset=utf-8'});
+		const url = URL.createObjectURL(blob);
+
+		const formData = new FormData();
+		formData.append('id', selected_meme.id);
+		formData.append('file', blob);
+		try {
+			const res = await fetch('?/upload', {
+				method: 'POST',
+				body: formData
+			});
+			const body = await res.json();
+			if(body.type=='redirect' && body.status==307) {
+				goto(body.location);
+			}
+		} catch(e) {
+			console.log(e);
+		}
+	}
+
 	const save = async(e)=>{
 		const d=await getFileAsBase64('/api/img/'+selected_meme.image);
 		const clone=document.getElementById('svg').cloneNode(true);
 		clone.getElementsByTagName('image')[0].setAttribute('href', 'data:'+d.t+';base64,'+d.d);
+
 		const data = (new XMLSerializer()).serializeToString(clone);
 		const blob = new Blob([data], {type: 'image/svg+xml;charset=utf-8'});
 		const url = URL.createObjectURL(blob);
 		
+		// TODO: Don't use a pre-existing dl_link. We can create an element & use that.
+		// However... we do need to store the href (so we can revoke it) otherwise its a mem leak.
 		const btnlink=document.getElementById('dl_link');
 		const hr=btnlink.getAttribute('href');
+
 		if(hr){URL.revokeObjectURL(hr)}
 		btnlink.setAttribute('download', (selected_meme.name.replaceAll(' ',''))+'.svg');
 		btnlink.setAttribute('target', '_blank');
 		btnlink.setAttribute('href', url);
 		btnlink.click();
-		// Let the event continue to propigate, and it'll DL
 	}
 
 	//$inspect(text_blocks);
@@ -84,20 +123,23 @@
 	<textarea cols='50' rows='3' bind:value={text_blocks[idx]} oninput={(e)=>{update_one(e,idx)}}>{block.text||''}</textarea>
 	{/each}
 	<label>
-		<input type='number' min='14' max='128' step='2' bind:value={font_size} onchange={update_all}/>
+		<input type='number' min='14' max='256' step='2' bind:value={font_size} onchange={update_all}/>
 		Font size
 	</label>
+	
+	<button onclick={post} style='margin-left:30px'>Post to ImgCat</button>
 	<button onclick={save} style='margin-left:30px'>Save meme as...</button>
 	<a id='dl_link' style='visibility:hidden'></a>
 	<br/><br/>
 
-	<svg id='svg' height={selected_meme.height} width={selected_meme.width}>
+	<!-- <svg id='svg' viewbox='0 0 {selected_meme.width} {selected_meme.height}' width={Math.min(1000/selected_meme.width,2.0)*selected_meme.width} height={Math.min(1000/selected_meme.width,2.0)*selected_meme.height}> -->
+	<svg id='svg' width={selected_meme.width} height={selected_meme.height}>
 		<style>text{font-family:sans-serif;font-weight:bold;fill:#FFF;stroke:#000;stroke-width:0.1em;paint-order:stroke;stroke-linejoin:round;text-anchor:middle;dominant-baseline:middle}</style>
 		<image href='/api/img/{selected_meme.image}' width='100%' />
 		{#each selected_meme.text as tb, i}
-		<text x={tb.x||'50%'} y={tb.y+font_size*(tb.dir?-0.5:0.5)} style='font-size:{font_size}px'>
+		<text x={tb.x||'50%'} y={(tb.y||(tb.dir?PADDING:selected_meme.height-PADDING))+font_size*(tb.dir?-0.5:0.5)} style='font-size:{font_size}px'>
 			{#each text_split[i] as ts, j}
-			<tspan x='50%' dy={(tb.dir?font_size:-font_size)}>{ts}</tspan>
+			<tspan x='50%' dy={(tb.dir?font_size:-font_size)*1.2}>{ts}</tspan>
 			{/each}
 		</text>
 		{/each}
