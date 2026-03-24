7a0f28e2cbfb354a8bdc90ec12abe6f208b194c5
Andy R. Schmitz <github@arschmitz.com>
2026-03-24 14:54:43 -0400

    Moved formatting to common func

--- frontend/src/lib/server/meme_maker.ts
+++ frontend/src/lib/server/meme_maker.ts
@@ -1,56 +1,62 @@
 import { query } from '$lib/server/dbpool.ts';
 
+function parseTemplateData(r) {
+	// Reverse-sort the lists, so we can .pop() efficiently
+	r[0].sort((a,b)=>{b.id-a.id});
+	r[1].sort((a,b)=>{(a.id!=b.id)?b.id-a.id:b.idx-a.idx});
+
+	const result = [];
+
+	let cur_template = r[0].pop();
+	let cur_text = r[1].pop();
+
+	// A simple for-loop might be faster, but I think this will be more memory-stable.
+	while(cur_template) {
+		if(!cur_template.thumb){ delete cur_template.thumb }
+		cur_template.text = [];
+
+		while(cur_template.id == cur_text?.id){
+			// This is a sparse table, so delete what we don't need
+			delete cur_text.id;  // Id is already in the header
+			delete cur_text.idx; // We don't care b/c the list is already sorted
+			cur_text.dir = Boolean(cur_text.dir); // Probably pointless
+			if(!cur_text.x){ delete cur_text.x }
+			if(!cur_text.y){ delete cur_text.y }
+			if(!cur_text.height){ delete cur_text.height }
+			if(!cur_text.width){ delete cur_text.width }
+			if(!cur_text.text){ delete cur_text.text }
+			if(!cur_text.angle){ delete cur_text.angle }
+
+			cur_template.text.push(cur_text);
+			cur_text = r[1].pop();
+		}				
+
+		// Reverse the list before pushing it (MUCH faster than unshift())
+		cur_template.text.reverse();
+		result.push(cur_template);
+		cur_template = r[0].pop();
+	}
+
+	// Reverse the list
+	return result.reverse();
+}
+
 export async function getTemplates():any {
 	return query(
 		"CALL MemeMaker.GetTemplates();",
 		[],
 		(r)=>{
-			// As long as the data is ordered by id (which it is, and is a
-			// hard requirement documented in the PROC), we can treat the
-			// arrays as queues & merge them efficiently w/o dicts/lookups.
-			// TODO: Shift() is not as efficient as pop(). Either build the
-			// loop backwards, then .reverse(). Or use traditional i/j loops.
+			return parseTemplateData(r);
+		}
+	);
+}
 
-			const result = [];
-
-			let cur_template = r[0].shift();
-			let cur_text = r[1].shift();
-
-			while(cur_template) {
-				if(!cur_template.thumb){ delete cur_template.thumb }
-				cur_template.text = [];
-
-				// This shouldn't ever happen... but would really mess things up
-				// TODO: Refactor the PROC to guarantee both halves use the same
-				// IDs within a temp table, etc. Then we can remove this check.
-				while(cur_template.id>cur_text?.id){
-					cur_text = r[1].shift();
-				}
-
-				while(cur_template.id==cur_text?.id){
-					delete cur_text.id;  // We already know the id's match
-					delete cur_text.idx; // We relying on SQL to sort & Arrays keep order
-					if(!cur_text.height){ delete cur_text.height }
-					if(cur_text.left && cur_text.right) {
-						// If there's left/right, convert to x & width
-						cur_text.width = cur_text.right - cur_text.left;
-						cur_text.x = cur_text.width / 2.0 + cur_text.left;
-						// TODO: We should handle a {left:50} and treate right as
-						// (width-left), etc. And also {right:50}, etc.
-					}
-					if(cur_text.left){ delete cur_text.left }
-					if(cur_text.right){ delete cur_text.right }
-					if(!cur_text.text){ delete cur_text.text }
-
-					cur_template.text.push(cur_text);
-					cur_text = r[1].shift();
-				}				
-
-				result.push(cur_template);
-				cur_template = r[0].shift();
-			}
-
-			return result;
+export async function getTemplateById(id:number):any {
+	return query(
+		"CALL MemeMaker.GetTemplateById(?);",
+		[id],
+		(r)=>{
+			return parseTemplateData(r);
 		}
 	);
 }
@@ -60,11 +66,29 @@ export async function getTemplateTags():any {
 		"CALL MemeMaker.GetTemplateTags();",
 		[],
 		(r)=>{
-			const result = r[0];
-			result.text = [];
-			for(const item in r[1]){
-				result.text.push(item);
+			// Reverse sort both lists
+			r[0].sort((a,b)=>{b.id-a.id});
+			r[1].sort((a,b)=>{b.id-a.id});
+
+			const result = new Map();
+
+			let cur_tag = r[0].pop();
+			let cur_map = r[1].pop();
+
+			// A for-loop is simpler, but this should be more memory-stable
+			while(cur_tag) {
+				cur_tag.templates = [];
+
+				while(cur_tag.id == cur_map?.id){
+					cur_tag.templates.push(cur_map.template);
+					cur_map = r[1].pop();
+				}
+
+				cur_tag.templates.reverse();
+				result.set(cur_tag.name, cur_tag.templates);
+				cut_tag = r[0].pop();
 			}
+
 			return result;
 		}
 	);
