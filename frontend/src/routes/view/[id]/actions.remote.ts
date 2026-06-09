121b4470900de4d1cea9b135a08fc1dcf9aafbf3
Andy R. Schmitz <github@arschmitz.com>
2026-06-09 12:50:16 -0400

    cleanup imports

--- frontend/src/routes/view/[id]/actions.remote.ts
+++ frontend/src/routes/view/[id]/actions.remote.ts
@@ -1,14 +1,10 @@
 import { number as req_number, strictTuple as req_tuple } from 'valibot';
-import { error } from '@sveltejs/kit';
-import { query, command } from '$app/server';
-import { getDbConn } from '$lib/server/dbpool.ts';
+import { query } from '$app/server';
 import * as Internal from '$lib/server/posts.ts';
 
 // NOTE: This relies on Svelte Remote Functions, which is EXPERIMENTAL
 // But they seem custom-designed to support server-side functions for components
 // See more: https://svelte.dev/docs/kit/remote-functions
-// 
-
 
 // NOTE: We're using the live refresh version, which may be flakey
 //export const getViews = query(req_number(), async (post_id) => {
@@ -31,8 +27,6 @@ export const liveVotes = query.live(req_number(), async function* (post_id) {
 		await new Promise((resolve) => setTimeout(resolve, 15000));
 	}
 });
-
-
 
 export const getMyVote = query(req_tuple([req_number(), req_number()]), async (args):number => {
 	const post_id = args[0];
