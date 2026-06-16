d5c1a4d8470f2dc48f00005ee655af132a6d36e3
Andy R. Schmitz <github@arschmitz.com>
2026-06-16 18:23:06 -0400

    Splitting the perm/temp ban checks into different calls

--- backend/libs/ic_actix/src/lib.rs
+++ backend/libs/ic_actix/src/lib.rs
@@ -11,7 +11,7 @@ pub use header_helpers::{get_bearer_jwt};
 #[cfg(all(feature="redis", feature="postgres"))]
 mod user_ban;
 #[cfg(all(feature="redis", feature="postgres"))]
-pub use user_ban::{check_ban};
+pub use user_ban::{check_temp_ban, check_perm_ban};
 
 #[cfg(feature="redis")] mod app_state_redis;
 #[cfg(feature="redis")] pub use app_state_redis::AppStateRedis;
--- backend/libs/ic_actix/src/user_ban.rs
+++ backend/libs/ic_actix/src/user_ban.rs
@@ -1,28 +1,29 @@
-use redis::Cmd;
+use redis::{Connection, Cmd};
+use deadpool_postgres::Client;
 
 use crate::ic_error::{ICError, ICResult};
-use crate::app_state_redis::AppStateRedis;
-use crate::app_state_postgres::AppStatePostgres;
+//use crate::app_state_redis::AppStateRedis;
+//use crate::app_state_postgres::AppStatePostgres;
 
-pub async fn check_ban(account_id:i64, redis:&AppStateRedis, pg:&AppStatePostgres) -> ICResult<()> {
-	// Check if they're banned in Redis (temporarially)
-	let Ok(mut conn) = redis.get_conn() else {
-		return Err(ICError::REDIS_CONN);
-	};
 
-	// Check in Redis, which stores temporary bans
+pub async fn check_temp_ban(account_id:i64, redis:&mut Connection) -> ICResult<()> {
 	let Ok(value) = Cmd::new()
 		.arg("EXISTS")
 		.arg(format!("ban:{}", account_id))
-		.query::<usize>(&mut conn) else { return Err(ICError::REDIS_CONN) };
+		.query::<usize>(redis) else { return Err(ICError::REDIS_CONN) };
 
 	if value > 0 {
 		return Err(ICError::BAN_TEMP);
 	}
 
+	// Ok, we're good!
+	return Ok(());
+}
+
+
+pub async fn check_perm_ban(account_id:i64, pg:&Client) -> ICResult<()> {
 	// Check in Postgres, which stores permanent bans
 	// TODO: everything
-	let _ = pg;
 
 	// Ok, we're good!
 	return Ok(());
