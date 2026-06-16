e177b3ed1569ea87c9566a28b80d7e3437893d23
Andy R. Schmitz <github@arschmitz.com>
2026-06-16 19:04:49 -0400

    Created new Comment controls (that actually nest properly). Kept old ones, for now.

---
+++ frontend/src/routes/view/[id]/Comment.svelte
@@ -0,0 +1,68 @@
+<script lang='ts'>
+	import { pretty_print_relative } from '$lib/time_fmt.ts';
+	import Comment from './Comment.svelte';
+	import Button from '$lib/Button.svelte';
+
+	let {
+		id,
+		comments,
+		replies,
+		reply_to = $bindable(),
+		level = 0,
+	} = $props();
+	const self = comments.get(id);
+	let open = $state( level<=0 ); // By deafult, open root comments & show the second layer
+</script>
+
+
+<div class='cmt'>
+	<label>TBD Username - {pretty_print_relative(self.ts, navigator.language)}</label>
+	<p>{self.text}</p>
+	
+	<div class='btngrp'>
+		<Button img='/chat.svg' lbl='Reply' class={(reply_to===id?'tbtn sel':undefined)} onclick={()=>reply_to=id} />
+
+		{#if replies.has(id)}
+			{#if open}
+				<Button lbl='Hide replies' onclick={()=>{open=false}} />
+			{:else}
+				<Button lbl='See replies ({replies.get(id).length})' onclick={()=>{open=true}} />
+			{/if}
+
+		{/if}
+	</div>
+	{#if open}
+		{#each replies.get(id) as inner_id}
+			<Comment id={inner_id} {comments} {replies} bind:reply_to level={level+1} />
+		{/each}
+	{/if}
+</div>
+
+<style>
+	div.cmt {
+		background-color: var(--cb3);
+		margin-top: 1.5em;
+		padding: 0.5em 0em 0.5em 1.5em;
+		border-left: 4px solid var(--cb1);
+
+		label, p {
+			display: block;
+		}
+		label {
+			color: var(--cticy);
+		}
+		div.btngrp {
+			margin-top:1em;
+		}
+
+	}
+	textarea {
+		display: block;
+		width: 100%;
+		min-width: 300px;
+		max-width: 800px;
+	}
+	input {
+		margin-bottom: 20px;
+	}
+</style>
---
+++ frontend/src/routes/view/[id]/CommentPanel.svelte
@@ -0,0 +1,72 @@
+<script lang='ts'>
+	import { pretty_print_relative } from '$lib/time_fmt.ts';
+	import Comment from './Comment.svelte';
+	import Button from '$lib/Button.svelte';
+	import Modal from '$lib/Modal.svelte';
+	import { enhance } from '$app/forms';
+
+	let {
+		post,
+		form,
+		user_id,
+		comment_replies,
+		reply_to = $bindable(null),
+	} = $props();
+
+	const {comments, replies} = $derived(await comment_replies);
+
+	let showCommentModal = $derived(Number.isInteger(reply_to));
+
+	function submit_success(e){
+		return async({result, update}) => {
+			console.log(e);
+			console.log(result);
+			await update();
+			// Do post-processing
+			if(form?.success) {
+				reply_to = null;
+			}
+		}
+	}
+</script>
+
+<!-- <div>
+	<label>Reply to this post:
+		<textarea bind:this={message_element} name='message' rows=4 maxlength=255 ></textarea>
+	</label>
+	{#if reply_to !== 0}
+	<Button img='/chat.svg' lbl='Comment' onclick={()=>reply_to=0} />
+	{:else}
+	<Button img='/chat.svg' lbl='Cancel' onclick={()=>reply_to=null} />
+	{/if}
+</div> -->
+{#each replies.get(0) as id}
+<div class='root-cmt'>
+<Comment {id} {comments} {replies} bind:reply_to />
+</div>
+{/each}
+
+<Modal id='comment' bind:showModal={showCommentModal} title='Connect with the community'>
+	<form method='POST' action='?/comment' use:enhance={submit_success} >
+		{#if reply_to>0}
+		<label style='color:var(--cticy)'>TBD Username - {pretty_print_relative(comments.get(reply_to).ts, navigator.language)}</label>
+		<p>{comments.get(reply_to).text}</p>
+		<input type='hidden' name='reply_to' value={reply_to} />
+		<br />
+		{/if}
+		<textarea name='comment' style='width:30em;height:5em'></textarea>
+		<br /><br />
+		<Button img='/chat.svg' lbl={(reply_to===0?'Comment':'Reply')} type='submit' />
+	</form>
+</Modal>
+
+<style>
+	div.root-cmt {
+		background-color: var(--cb3);
+		margin-top: 1em;
+
+		label, textarea {
+			display: block;
+		}
+	}
+</style>
