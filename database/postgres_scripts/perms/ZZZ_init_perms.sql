ff60b6f589267abcc034fc46a436104637d60a81
Andy R. Schmitz <github@arschmitz.com>
2026-07-17 14:24:34 -0400

    documentation & cleanup

--- database/postgres_scripts/perms/ZZZ_init_perms.sql
+++ database/postgres_scripts/perms/ZZZ_init_perms.sql
@@ -1,15 +1,18 @@
--- IC_UDB_USER is the user for the Auth Microservice
-
+-- Auth Microservice
 GRANT USAGE ON SCHEMA UserDB TO IC_UDB_USER;
 GRANT EXECUTE ON FUNCTION UserDB.GetAccountId TO IC_UDB_USER;
 GRANT EXECUTE ON FUNCTION UserDB.GetAccountData TO IC_UDB_USER;
-GRANT EXECUTE ON FUNCTION UserDB.GetAccountPreferences TO IC_UDB_USER;
 GRANT EXECUTE ON FUNCTION UserDB.CreateAccount TO IC_UDB_USER;
 GRANT EXECUTE ON FUNCTION UserDB.IsUsernameFree TO IC_UDB_USER;
 
 GRANT USAGE ON SCHEMA Legal TO IC_UDB_USER;
 GRANT EXECUTE ON FUNCTION Legal.IsAgeNeededOnSignup TO IC_UDB_USER;
 
+-- Posts Microservice
+-- TODO: It's currently using IC_UDB_USER, not it's own service account
+
+-- Softmod Microservice
+-- N/A - All functionality currently uses MariaDB
 
 -- Users Microservice
 GRANT USAGE ON SCHEMA UsersSvc TO IC_USERS_SVC_USER;
