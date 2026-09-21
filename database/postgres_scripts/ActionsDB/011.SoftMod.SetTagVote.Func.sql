a72dfc02646e27dce4ac7029938752eb41813025
Andy R. Schmitz <github@arschmitz.com>
2026-09-20 20:26:59 -0400

    Changed SetTagVote to take an array of tags, instead of calling it once-per-input

--- database/postgres_scripts/ActionsDB/011.SoftMod.SetTagVote.Func.sql
+++ database/postgres_scripts/ActionsDB/011.SoftMod.SetTagVote.Func.sql
@@ -1,53 +1,71 @@
 CREATE OR REPLACE FUNCTION SoftMod.SetTagVote(
 	post_id BIGINT,
 	user_id BIGINT,
-	tag     TEXT
+	tags    TEXT[]
 )
 RETURNS BOOL
-LANGUAGE PLPGSQL
+LANGUAGE SQL
 STRICT
 SECURITY DEFINER
-AS $$
-DECLARE
-	v_tag_id BIGINT := (SELECT id FROM Public.Tag WHERE name = SetTagVote.tag);
-BEGIN
-
-	-- Check if the tag exists
-	IF v_tag_id IS NULL THEN
-		INSERT INTO Public.Tag (name)
-		VALUES (SetTagVote.tag)
+BEGIN ATOMIC
+	-- Figure out if tags exist
+	WITH tag_map(tag, tag_id) AS (
+		SELECT
+			a.a,
+			b.id
+		FROM unnest(Test.tags) a
+		LEFT JOIN Public.Tag b
+			ON a.a = b.name
+	),
+	
+	-- Anything that's new, insert it
+	new_tags(tag, tag_id) AS (
+		INSERT INTO Public.Tag(name)
+		SELECT tag
+		FROM tag_map
+		WHERE tag_id IS NULL
 		ON CONFLICT(name) DO NOTHING
-		RETURNING id
-		INTO v_tag_id;
+		RETURNING name, id
+	),
+	
+	-- Join existing & new tags
+	all_tags(tag, tag_id) AS (
+		SELECT tag, tag_id
+		FROM tag_map
+		WHERE tag_id IS NOT NULL
 
-		-- There is a race condition when two users insert the same tag at the same time
-		-- It won't error, but tag_id remains null (DO NOTHING has precident over RETURNING?)
-		IF v_tag_id IS NULL THEN
-			v_tag_id := (SELECT id FROM Public.Tag WHERE name = SetTagVote.tag);
-		END IF;
-	END IF;
+		UNION ALL
+		
+		SELECT tag, tag_id
+		FROM new_tags
+	),
 
+	-- TODO: This should be a MERGE
+	delete_old_entries AS (
+		DELETE FROM SoftMod.TagVote
+		WHERE post_id = Test.post_id
+			AND user_id = Test.user_id
+			AND tag_id NOT IN (
+				SELECT tag_id
+				FROM tag_map
+			)
+	)
 
+	-- Now, finally upsert
 	INSERT INTO SoftMod.TagVote AS a (
 		post_id,
 		user_id,
 		tag_id
 	)
-	VALUES(
-		SetTagVote.post_id,
-		SetTagVote.user_id,
-		v_tag_id
-	)
+	SELECT
+		Test.post_id,
+		Test.user_id,
+		a.tag_id
+	FROM all_tags a
 	ON CONFLICT
 		ON CONSTRAINT tagvote_pkey
 		DO UPDATE
-		SET
-			upload_time = CURRENT_TIMESTAMP,
-			post_id = SetTagVote.post_id,
-			user_id = SetTagVote.user_id,
-			tag_id = v_tag_id;
+		SET upload_time = CURRENT_TIMESTAMP;
 
-	-- No reason this can fail, always return true
-	RETURN TRUE;
+	SELECT TRUE;
 END;
-$$;
