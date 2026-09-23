1df1eae2e77e4febdeb00667d00b9f1973efcf98
Andy R. Schmitz <github@arschmitz.com>
2026-09-23 04:11:35 -0400

    documentation

--- database/postgres_scripts/perms/ActionsDB.perms.sql
+++ database/postgres_scripts/perms/ActionsDB.perms.sql
@@ -3,12 +3,14 @@
 --    process is so dependent on implementation details & secrets    --
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 
--- Posts is the older, all-in one, MariaDB-based service. Actions is the newer, separated service.
---   We want to separate the actions into its own microservice because they're write-heavy, whereas posts are almost entirely read-only
---   That will create scalability issues later when we do regional, RO-clones, etc. that doesn't work with writes.
--- The Maturity Service is a placeholder until we figure out the design pattern. But it's a database daemon service, grabbing
--- the list of new votes, and rerunning the calculation. It's not an interactive, data-driven REST service like everything else.
--- 
+-- Actions is the newer, separated service (from Posts). We want to separate the
+-- actions into its own microservice because they're write-heavy, whereas posts
+-- are almost entirely read-only. That will create scalability issues later when
+-- we do regional, RO-clones, etc. that doesn't work with writes.
+-- The Maturity Service is a placeholder until we figure out the design pattern. But
+-- it's a database daemon service, grabbing the list of new votes, and rerunning the
+-- calculation. It's not an interactive, data-driven REST service like everything else.
+
 CREATE USER ${IC_POSTS_SVC_USER} WITH PASSWORD '${IC_POSTS_SVC_PASS}';
 CREATE USER ${IC_ACTIONS_SVC_USER} WITH PASSWORD '${IC_ACTIONS_SVC_PASS}';
 CREATE USER ${IC_MATURITY_SVC_USER} WITH PASSWORD '${IC_MATURITY_SVC_PASS}';
